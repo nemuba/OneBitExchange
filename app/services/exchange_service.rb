2d76f1e9265b484da31243e438411456d8536a33
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-11-05 06:54:25 -0200

    setup for deploy

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -10,8 +10,8 @@ class ExchangeService
 
   def perform
     begin
-      exchange_api_url = Rails.application.credentials.dig(:test, :currency_api_url)
-      exchange_api_key = Rails.application.credentials.dig(:test,:currency_api_key)
+      exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
+      exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
       url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
       res = RestClient.get url
       value = JSON.parse(res.body)['currency'][0]['value'].to_f
